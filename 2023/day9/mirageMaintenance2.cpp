0a01b7b47d54df6ce578bc6afb0bef4696e7de20
Alexandru Herlas <alexandru.herlas@protektwar>
2023-12-17 07:35:27 +0100

    added 2023/day9 solutions

---
+++ 2023/day9/mirageMaintenance2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <map>
+#include <algorithm>
+#include <ranges>
+#include <cstddef>        // std::size_t
+
+int main(void)
+{
+    std::ifstream inFile;
+    std::string line, numberStr;
+
+    inFile.open("input");
+//    inFile.open("test");
+    int sum = 0;
+    std::vector<int> temp; 
+    std::vector<int> temp1; 
+    std::vector<std::vector<std::vector<int>>> serieVectors; 
+    std::vector<std::vector<int>> seriesVector; 
+    long long int extrapolatedValuesSum = 0;
+
+    for( std::string line; getline( inFile, line ); ){
+      std::stringstream series(line);
+      temp.clear();
+      seriesVector.clear();
+      while (getline(series, numberStr, ' ')){
+        if (numberStr != ""){ 
+          temp.push_back(std::stoi(numberStr));
+        }
+      }
+      do {
+        sum = 0;
+        seriesVector.push_back(temp);
+        for (int i = 0; i < temp.size()-1; i++){
+          temp1.push_back(temp.at(i+1) - temp.at(i));
+          sum = sum + (temp.at(i+1) - temp.at(i));
+        }
+        temp.clear();
+        temp.assign(temp1.begin(), temp1.end());
+        temp1.clear();
+      } while (sum != 0);
+      serieVectors.push_back(seriesVector);
+      std::reverse(seriesVector.begin(), seriesVector.end());
+      int numberA = 0;
+      for(std::vector<int> s_vector: seriesVector){
+        numberA = s_vector.front() - numberA; 
+      }
+      extrapolatedValuesSum = extrapolatedValuesSum + numberA;
+    }
+
+
+
+    
+    std::cout << "What is the sum of these extrapolated values? "; 
+    std::cout << extrapolatedValuesSum << '\n';
+    inFile.close();
+}
